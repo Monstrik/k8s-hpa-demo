d73bac7d4e19f5f28719368e77c5a6eeec0980e8
ayakovis <ayakovis@objectsystems.com>
2026-04-20 10:28:39 -0400

    add informational message to CPU burn script for HPA tracking

--- 06-burn-cpu-inside-nginx.sh
+++ 06-burn-cpu-inside-nginx.sh
@@ -5,6 +5,7 @@ NAMESPACE=test
 APP=nginx
 
 echo "🔥 Burning CPU inside nginx pods (this WILL trigger HPA)..."
+echo "Open Freelens or track with verify scripts (U WILL see the scaling and wait 5 min for descaling after stop)"
 echo "Press Ctrl+C to stop and allow scale-down."
 
 kubectl exec -n ${NAMESPACE} -it deploy/${APP} -- \
